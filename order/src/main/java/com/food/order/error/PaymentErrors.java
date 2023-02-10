fdc50a3bab4df080edeaa8a5d834e30f3beae98b
olivejua <tmfrl4710@gmail.com>
2023-02-10 20:36:59 +0900

    refactor: ApplicationErrors 추가 및 OrderErrors, PaymentErrors 구현

--- order/src/main/java/com/food/order/error/PaymentErrors.java
+++ order/src/main/java/com/food/order/error/PaymentErrors.java
@@ -1,26 +1,21 @@
 package com.food.order.error;
 
+import com.food.common.error.ApplicationErrors;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public enum PaymentErrors {
+public enum PaymentErrors implements ApplicationErrors {
     INVALID_PAYMENT_PARAMETERS("P001", "유효하지 않은 파라미터 요청으로 인해 결제를 수행할 수 없습니다."),
     ;
 
     private final String code;
-    private String message;
+    private final String message;
 
     public String getCode() {
         return code;
     }
 
     public String getMessage() {
-        return message;
-    }
-
-    public String appendMessage(String extraMessage) {
-        message = String.format("%s \n%s", message, extraMessage);
-
         return message;
     }
 }
