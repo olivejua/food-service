3bf9f42e42618bc94321b1997ba01c1aaf57f902
olivejua <tmfrl4710@gmail.com>
2023-02-13 18:12:57 +0900

    style : commonService interface에 주석 추가

--- common/src/main/java/com/food/common/order/business/internal/OrderCommonService.java
+++ common/src/main/java/com/food/common/order/business/internal/OrderCommonService.java
@@ -5,15 +5,25 @@ import com.food.common.order.business.internal.dto.OrderDto;
 import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
-/**
- * Repository에서 별다른 비즈니스 로직을 포함시키지 않은 채
- * Entity를 직접접근을 방지하기 위해 DTO로만 리턴해준다.
- */
-
 public interface OrderCommonService {
+    /**
+     * OrderId로 주문데이터를 단건 조회한다.
+     * @param id 검색할 ID. null일 수 없다.
+     * @return ID로 조회해온 주문데이터가 담긴 Dto 객체
+     */
     Optional<OrderDto> findById(@NotNull Long id);
 
+    /**
+     * OrderId로 주문데이터의 존재유무를 리턴한다.
+     * @param id 검색할 ID. null일 수 없다.
+     * @return 데이터 존재 유무
+     */
     boolean existsById(@NotNull Long id);
 
+    /**
+     * 주문데이터를 저장하고, 새로운 ID가 부여된 Dto 객체를 리턴한다.
+     * @param order 저장될 주문데이터. null일 수 없다.
+     * @return 저장된 주문 데이터가 담긴 Dto 객체. 리턴객체는 null일 수 없다.
+     */
     OrderDto save(OrderDto order);
 }
--- common/src/main/java/com/food/common/payment/business/internal/PaymentCommonService.java
+++ common/src/main/java/com/food/common/payment/business/internal/PaymentCommonService.java
@@ -1,13 +1,24 @@
 package com.food.common.payment.business.internal;
 
+import com.food.common.payment.business.internal.model.PaymentDto;
 import com.food.common.payment.enumeration.PaymentActionType;
 
 import javax.validation.constraints.NotNull;
 
 public interface PaymentCommonService {
-    Long save(@NotNull Long orderId, @NotNull PaymentActionType actionType);
+    /**
+     * 결제데이터를 저장하고, 새로운 ID가 부여된 Dto 객체를 리턴한다.
+     * @param paymentDto 저장될 결제데이터. null일 수 없다.
+     * @return 저장된 결제 데이터가 담긴 Dto 객체
+     */
+    PaymentDto save(@NotNull PaymentDto paymentDto);
 
     void updateActionType(@NotNull Long paymentId, @NotNull PaymentActionType actionType);
 
+    /**
+     * PaymentId로 결제데이터의 존재유무를 리턴한다.
+     * @param id 검색할 ID. null일 수 없다.
+     * @return 데이터 존재 유무
+     */
     boolean existsById(@NotNull Long id);
 }
