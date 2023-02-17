e63feead429dc35b0b792942aa1c80b5b5f0de14
olivejua <tmfrl4710@gmail.com>
2023-02-17 18:21:46 +0900

    style: PayService 인터페이스의 주석 작성

--- order/src/test/java/com/food/order/temp/PayService.java
+++ order/src/test/java/com/food/order/temp/PayService.java
@@ -1,11 +1,24 @@
 package com.food.order.temp;
 
 import com.food.common.user.business.external.model.RequestUser;
-import com.food.order.mock.MockRequestUser;
 import com.food.order.presentation.dto.request.PaymentDoRequest;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public interface PayService {
+    /**
+     * 주문건의 결제정보를 저장하고, 결제정보의 ID를 리턴한다.
+     * @param request 주문건과 결제 수단을 포함한 요청 정보. null일 수 없다.
+     * @param requestUser 결제 요청자. null일 수 없다.
+     * @return 결제정보 ID
+     */
     Long pay(PaymentDoRequest request, RequestUser requestUser);
 
-    void cancel(Long paymentId, MockRequestUser mockRequestUser);
+    /**
+     * 결제정보 취소를 저장한다.
+     * 취소하는 결제정보에 포인트 사용/적립 존재유무에 따라 재적립/회수절차가 포함된다.
+     * @param paymentId 결제정보 ID. null일 수 없다.
+     * @param requestUser 결제 취소 요청자. null일 수 없다.
+     */
+    void cancel(Long paymentId, RequestUser requestUser);
 }
