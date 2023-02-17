a46c1dbc7f958aaa8d21f16776d1f60329c391bd
olivejua <tmfrl4710@gmail.com>
2023-02-17 19:06:59 +0900

    refactor: PaymentLogRepository 사용하지 않는 조회 메서드 정리

--- common/src/main/java/com/food/common/payment/repository/PaymentLogRepository.java
+++ common/src/main/java/com/food/common/payment/repository/PaymentLogRepository.java
@@ -8,6 +8,4 @@ import java.util.List;
 
 public interface PaymentLogRepository extends JpaRepository<PaymentLog, Long> {
     List<PaymentLog> findAllByPayment(Payment payment);
-
-    List<PaymentLog> findAllByPaymentId(Long paymentId);
 }
