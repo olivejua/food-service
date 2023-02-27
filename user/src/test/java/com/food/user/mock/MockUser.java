43ff1f13c851be9eebaaa6062d1cb5640546314e
olivejua <tmfrl4710@gmail.com>
2023-02-27 23:59:23 +0900

    test: MockUser builder패턴 제거,  MockPayment 클래스 제거

--- user/src/test/java/com/food/user/mock/MockUser.java
+++ user/src/test/java/com/food/user/mock/MockUser.java
@@ -1,35 +1,21 @@
 package com.food.user.mock;
 
 import com.food.common.user.business.internal.dto.UserDto;
+import lombok.Builder;
 
-public class MockUser {
-    public static Builder builder() {
-        return new Builder();
+public class MockUser extends UserDto {
+
+    @Builder(builderClassName = "TestBuilder", builderMethodName = "testBuilder")
+    public MockUser(Long id, String nickname) {
+        super();
+
+        this.id = id;
+        this.nickname = nickname;
     }
 
-    public static UserDto create() {
-        return builder().build();
-    }
-
-    public static class Builder {
-        private Long id;
-        private String nickname = "TestUser";
-
-        public Builder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder nickname(String nickname) {
-            this.nickname = nickname;
-            return this;
-        }
-
-        public UserDto build() {
-            return UserDto.builder()
-                    .id(id)
-                    .nickname(nickname)
-                    .build();
-        }
+    public static MockUser create() {
+        return MockUser.testBuilder()
+                .nickname("TestUser")
+                .build();
     }
 }
