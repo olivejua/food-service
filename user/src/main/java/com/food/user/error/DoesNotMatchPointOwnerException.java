e3ccc2eea1aac10a5aefaa15f4b9405ab68eab64
olivejua <tmfrl4710@gmail.com>
2023-02-27 23:58:24 +0900

    feat: 포인트 재적립관련 Exception 클래스 생성

---
+++ user/src/main/java/com/food/user/error/DoesNotMatchPointOwnerException.java
@@ -0,0 +1,10 @@
+package com.food.user.error;
+
+import com.food.common.error.ApplicationErrors;
+
+public class DoesNotMatchPointOwnerException extends RuntimeException {
+    private static final ApplicationErrors ERROR = PointErrors.DOES_NOT_MATCH_POINT_OWNER;
+
+    public DoesNotMatchPointOwnerException() {
+    }
+}
---
+++ user/src/main/java/com/food/user/error/NotFoundPointException.java
@@ -0,0 +1,11 @@
+package com.food.user.error;
+
+import com.food.common.error.ApplicationErrors;
+
+public class NotFoundPointException extends RuntimeException {
+    private static final ApplicationErrors ERROR = PointErrors.NOT_FOUND_POINT;
+
+    public NotFoundPointException(Long pointId) {
+        super(ERROR.appendMessage("pointId=" + pointId));
+    }
+}
