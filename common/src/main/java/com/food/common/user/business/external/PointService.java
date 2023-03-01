c2d02dcd1416737aef87f9395bbe02bb8e9246a4
olivejua <tmfrl4710@gmail.com>
2023-03-01 21:55:31 +0900

    style: PointService 주석 작성

--- common/src/main/java/com/food/common/user/business/external/PointService.java
+++ common/src/main/java/com/food/common/user/business/external/PointService.java
@@ -19,7 +19,7 @@ public interface PointService {
      * 포인트를 사용한다.
      * @param amount 사용 포인트 금액이 담긴 객체
      * @param requestUser 포인트 차감 대상 소유자. 현재 로그인한 유저가 포인트 차감 대상이 된다.
-     * @return
+     * @return 사용 포인트 ID
      */
     Long use(@NotNull UsedPoints amount, @NotNull RequestUser requestUser);
 
@@ -27,6 +27,7 @@ public interface PointService {
      * 사용했던 포인트를 재적립한다.
      * @param pointId 사용포인트 ID
      * @param requestUser 포인트 재적립 대상 소유자. 현재 로그인한 유저가 포인트 재적립 대상이 된다.
+     * @return 재적립 포인트 ID
      */
     Long recollect(@NotNull Long pointId, @NotNull RequestUser requestUser);
 
@@ -34,6 +35,7 @@ public interface PointService {
      * 주어진 결제ID로 적립된 포인트 금액을 회수한다.
      * @param paymentId 적립된 결제 ID
      * @param requestUser 포인트 회수 대상 소유자. 현재 로그인한 유저가 포인트 재적립 대상이 된다.
+     * @return 회수 포인트 ID
      */
     Long retrieve(@NotNull Long paymentId, @NotNull RequestUser requestUser);
 }
