3bbfab9432ea4c0e05ded2fb77eb45e26608b6bc
olivejua <tmfrl4710@gmail.com>
2023-03-01 21:45:26 +0900

    refactor: DoesNotMatchPointOwnerException 에러메시지 설정

--- user/src/main/java/com/food/user/error/DoesNotMatchPointOwnerException.java
+++ user/src/main/java/com/food/user/error/DoesNotMatchPointOwnerException.java
@@ -6,5 +6,6 @@ public class DoesNotMatchPointOwnerException extends RuntimeException {
     private static final ApplicationErrors ERROR = PointErrors.DOES_NOT_MATCH_POINT_OWNER;
 
     public DoesNotMatchPointOwnerException() {
+        super(ERROR.getMessage());
     }
 }
