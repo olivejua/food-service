a935cb541bef3b419f5d871bf199e1549364cfcf
olivejua <tmfrl4710@gmail.com>
2023-03-03 22:12:49 +0900

    refactor: OrderErrors.NOT_FOUND_STORE_TO_ORDER 상수 추가

--- order/src/main/java/com/food/order/error/OrderErrors.java
+++ order/src/main/java/com/food/order/error/OrderErrors.java
@@ -5,7 +5,8 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum OrderErrors implements ApplicationErrors {
-    NOT_FOUND_ORDER("ORDER_0001", "주문서를 찾을 수 없습니다.")
+    NOT_FOUND_ORDER("ORDER_0001", "주문서를 찾을 수 없습니다."),
+    NOT_FOUND_STORE_TO_ORDER("ORDER_0002", "주문할 가게를 찾을 수 없습니다."),
     ;
 
     private final String code;
