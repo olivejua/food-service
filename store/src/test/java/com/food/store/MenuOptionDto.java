b49a4794f24a2b90c45cd124d69bd343ed5ad057
olivejua <tmfrl4710@gmail.com>
2023-03-05 15:10:27 +0900

    feat: MenuOptionDto, MenuSelectionDto 클래스 생성

---
+++ store/src/test/java/com/food/store/MenuOptionDto.java
@@ -0,0 +1,23 @@
+package com.food.store;
+
+import com.food.common.menu.domain.MenuOption;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+public class MenuOptionDto {
+    private Long id;
+    private Long menuId;
+    private String name;
+    private Byte minSize;
+    private Byte maxSize;
+
+    public MenuOptionDto(@NotNull MenuOption entity) {
+        this.id = entity.getId();
+        this.menuId = entity.getMenuId();
+        this.name = entity.getName();
+        this.minSize = entity.getMinSize();
+        this.maxSize = entity.getMaxSize();
+    }
+}
