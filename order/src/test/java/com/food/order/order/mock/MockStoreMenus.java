5907a3d6edd863262705d4afd2c9821e25eaada5
olivejua <tmfrl4710@gmail.com>
2023-03-14 23:31:47 +0900

    test: MockStoreMenus 클래스 생성

---
+++ order/src/test/java/com/food/order/order/mock/MockStoreMenus.java
@@ -0,0 +1,36 @@
+package com.food.order.order.mock;
+
+import com.food.common.menu.business.external.dto.StoreMenus;
+import com.food.common.menu.business.internal.dto.MenuDtoWithRelations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockStoreMenus implements StoreMenus {
+    private Long storeId;
+    private List<MenuDtoWithRelations> menus = new ArrayList<>();
+
+    public static MockStoreMenus create(Long storeId, List<MenuDtoWithRelations> menus) {
+        MockStoreMenus newInstance = new MockStoreMenus();
+        newInstance.storeId = storeId;
+        newInstance.menus.addAll(menus);
+
+        return newInstance;
+    }
+
+    @Override
+    public Long getStoreId() {
+        return storeId;
+    }
+
+    @Override
+    public List<MenuDtoWithRelations> getMenus() {
+        return menus;
+    }
+
+    @Override
+    public boolean containsAll(List<Long> menuIds) {
+        return menus.stream()
+                .allMatch(menu -> menuIds.contains(menu.getId()));
+    }
+}
