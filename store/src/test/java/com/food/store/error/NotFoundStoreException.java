9f8a617c8f00ce2dc34f6beb4349be4a335f2467
olivejua <tmfrl4710@gmail.com>
2023-03-18 17:43:53 +0900

    test: Mock MenuItem 생성

--- store/src/test/java/com/food/store/error/NotFoundStoreException.java
+++ store/src/test/java/com/food/store/error/NotFoundStoreException.java
@@ -6,6 +6,6 @@ public class NotFoundStoreException extends RuntimeException {
     private static final ApplicationErrors ERROR = MenuErrors.NOT_FOUND_STORE_TO_FIND_MENUS;
 
     public NotFoundStoreException(Long storeId) {
-        super(ERROR.appendMessage("storeId=" + storeId));
+        super("storeId=" + storeId);
     }
 }
